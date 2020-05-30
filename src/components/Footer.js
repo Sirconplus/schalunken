f87282c97e41d1a25b360f5df9223bd842dd9bf2
Niklas Eicker <git@nikl.me>
2020-05-31 00:49:06 +0200

    Adopt footer and episode description css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,26 +14,24 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns">
-              <div className="column is-8">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="twitter" href="https://twitter.com/schalunken">
-                  <img className="fas fa-lg" src={twitter} alt="Twitter" style={{ width: '1em', height: '1em' }} />
-                </a>
+              <div className="column is-12">
+                <ul className="footer-list">
+                  <li>
+                    <Link to="/" className="navbar-item">
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="navbar-item" to="/about">
+                      <span>About</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <a title="twitter" href="https://twitter.com/schalunken">
+                      <img className="fas fa-lg" src={twitter} alt="Twitter" style={{ width: '1em', height: '1em' }} />
+                    </a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
