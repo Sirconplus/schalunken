b9dd2c364715e03073707991a01879a0b9275ffc
Niklas Eicker <git@nikl.me>
2020-06-05 17:22:37 +0200

    Rewrite and use ID widget

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,28 +1,13 @@
 import CMS from 'netlify-cms-app';
-import { v4 as uuidv4 } from 'uuid';
 
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import PodcastEpisodePreview from './preview-templates/PodcastEpisodePreview';
 import React from 'react';
+import UuidWidget from './customWidgets/uuid/Controll';
+import UuidPreview from './customWidgets/uuid/Preview';
 
 CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('podcast', PodcastEpisodePreview);
 
-const UuidWidget = (props) => {
-  if (props.value === undefined || props.value === '') {
-    props.value = uuidv4();
-  }
-  return (
-    <input
-      id={props.forID}
-      className={props.classNameWrapper}
-      type={'text'}
-      value={props.value}
-      onChange={() => {}}
-      disabled
-    />
-  );
-};
-
-CMS.registerWidget('id', UuidWidget);
+CMS.registerWidget('id', UuidWidget, UuidPreview);
 CMS.init();
